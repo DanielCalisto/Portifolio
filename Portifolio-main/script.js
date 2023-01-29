e2c05c103c271a340d9f510c7d61bd0ea6449227
DanielCalisto <118861762+DanielCalisto@users.noreply.github.com>
2023-01-28 23:58:11 -0300

    Add files via upload

---
+++ Portifolio-main/script.js
@@ -0,0 +1,64 @@
+function divA(){
+
+    let parte1 = document.getElementById("parte1")
+
+    if(parte1.style.display === "none") {
+        parte1.style.display = "block";
+    }else {
+        parte1.style.display = "none";
+    }}
+   
+
+function divB(){
+    let div2 = document.getElementById("skills")
+    if(div2.style.display === "none") {
+        div2.style.display = "block";
+        } else {
+        div2.style.display = "none";
+}}
+
+
+function divC(){
+let cursos = document.getElementById("cursos")
+
+if(cursos.style.display === "none") {
+    cursos.style.display = "block";
+} else {
+    cursos.style.display = "none";
+}}
+
+function divA1() {
+    let btnb = document.getElementById("skills")
+    if (btnb.style.display === "block") {
+        btnb.style.display = "none";
+    }else {}}
+function divA2() {
+    let btnc = document.getElementById("cursos")
+    if (btnc.style.display === "block") {
+        btnc.style.display = "none";
+    }else {btnc.style.display = "none" }}
+
+function divB1() {
+    let btna = document.getElementById("parte1")
+    if (btna.style.display === "block") {
+        btna.style.display = "none";
+    }else {    
+    }}
+function divB2() {
+        let btnc = document.getElementById("cursos")
+        if (btnc.style.display === "block") {
+            btnc.style.display = "none";
+        }else {    
+        }}
+function divC1() {
+            let btna = document.getElementById("parte1")
+            if (btna.style.display === "block") {
+                btna.style.display = "none";
+            }else {    
+            }}
+function divC2() {
+                let btnb = document.getElementById("skills")
+                if (btnb.style.display === "block") {
+                    btnb.style.display = "none";
+                }else {    
+                }}
